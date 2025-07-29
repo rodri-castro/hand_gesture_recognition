a4d45531189f75a3f837469623e94d1fefb303a7
Rodrigo Castro Ochoa <rcastro@uma.es>
2025-07-29 13:03:11 +0200

    .

--- install.sh
+++ install.sh
@@ -1,22 +1,22 @@
-#!/bin/bash
+# #!/bin/bash
 
-echo "╔══╣ Install: hand_gesture_recognition (STARTING) ╠══╗"
+# echo "╔══╣ Install: hand_gesture_recognition (STARTING) ╠══╗"
 
 
-# Install dependencies
-DIR = $(pwd)
-cd ..
-git clone https://github.com/TeamSOBITS/sobits_msgs
+# # Install dependencies
+# DIR = $(pwd)
+# cd ..
+# git clone https://github.com/TeamSOBITS/sobits_msgs
 
-cd $DIR
+# cd $DIR
 
-sudo apt-get update
-sudo apt-get install -y \
-    python3.9 \
-    python3.9-dev
+# sudo apt-get update
+# sudo apt-get install -y \
+#     python3.9 \
+#     python3.9-dev
 
-python3.9 -m pip install --upgrade pip
-python3.9 -m pip install mediapipe
-python3.9 -m pip install tensorflow
+# python3.9 -m pip install --upgrade pip
+# python3.9 -m pip install mediapipe
+# python3.9 -m pip install tensorflow
 
-echo "╚══╣ Install: hand_gesture_recognition (FINISHED) ╠══╝"
+# echo "╚══╣ Install: hand_gesture_recognition (FINISHED) ╠══╝"
